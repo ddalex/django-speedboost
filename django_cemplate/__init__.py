31714da54317129f00e0fd24580b6f51f4073bcb
Alex Damian <ddalex@gmail.com>
2015-12-08 16:51:56 +0000

    hooks the import to replace modules
    adding PEP302 hooks for the import call to replace
    the django.template.* with django_cemplate imports
    for the replaced modules
    
    bumped version number to 0.98

--- django_cemplate/__init__.py
+++ django_cemplate/__init__.py
@@ -1,2 +1,61 @@
 # -*- encoding:utf-8 -*-
 from __future__ import absolute_import, division, unicode_literals, print_function
+
+import importlib
+import sys
+
+MODULE_NAMES = ["context", "context_processors", "base", "defaulttags"]
+
+
+for module_name in MODULE_NAMES:
+    assert "django.template.%s" % module_name not in sys.modules
+
+
+class DjangoCemplateImporter():
+
+    original_template_modules = ["django.template.%s" % module_name for module_name in MODULE_NAMES]
+    replcmnt_template_modules = ["django_cemplate.%s" % module_name for module_name in MODULE_NAMES]
+    paths = {}
+
+    def find_module(self, full_name, path=None):
+        if full_name in self.original_template_modules:
+            self.paths[full_name] = path
+            return self
+        if full_name.startswith("django_cemplate.") and full_name not in self.replcmnt_template_modules:
+
+            sys.meta_path.remove(_instance)
+            try:
+                importlib.import_module(full_name.replace("django_cemplate.", "django.template."))
+            except ImportError:
+                return None
+            finally:
+                sys.meta_path.append(_instance)
+
+            return self
+        return None
+
+    def load_module(self, name):
+        if name in sys.modules:
+            return sys.modules[name]
+        if name in self.original_template_modules:
+            global _instance
+            imported_module = importlib.import_module(name.replace("django.template", "django_cemplate"))
+            sys.modules[name] = imported_module
+            return imported_module
+        elif name.startswith("django_cemplate.") and name not in self.replcmnt_template_modules:
+            m_name = name.replace("django_cemplate", "django.template")
+            global _instance
+            sys.meta_path.remove(_instance)
+            imported_module = importlib.import_module(m_name)
+            sys.meta_path.append(_instance)
+            sys.modules[name] = imported_module
+            return imported_module
+
+        raise ImportError("Could not load %s" % name)
+
+
+_instance = None
+_instance = DjangoCemplateImporter()
+
+if _instance is not None:
+    sys.meta_path.append(_instance)
--- setup.py
+++ setup.py
@@ -1,67 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
-import shutil
-import importlib
-
-from distutils.core import setup, Extension
-from Cython.Compiler.Main import compile
+from distutils.core import setup
 from Cython.Build import cythonize
 
 from Cython.Compiler.Options import directive_defaults
 
+from distutils import sysconfig
+site_packages_path = sysconfig.get_python_lib()
+
 directive_defaults['linetrace'] = True
 directive_defaults['binding'] = True
-
-
-class CythonModuleCompiler(list):
-    def __init__(self):
-        self.module_list = ['django.template.base', 'django.template.context']
-        self._cache = {}
-
-    def _mangle_module(self, module_name):
-        if module_name in self._cache:
-            return self._cache[module_name]
-
-        source_file_name = importlib.import_module(module_name).__file__
-        if source_file_name.endswith(".pyc"):
-            source_file_name = source_file_name[:-1]
-        if not source_file_name.endswith(".py"):
-            return
-        base_name = os.path.basename(source_file_name)
-
-        # generate the C files if they are not there
-        if not os.path.exists(os.path.join("django_cemplate", base_name.replace(".py", ".c"))):
-            shutil.copy2(source_file_name, "django_cemplate")
-            c_file_name = compile(os.path.join("django_cemplate", base_name)).c_file
-            os.remove(os.path.join("django_cemplate", base_name))
-        else:
-            c_file_name = os.path.join("django_cemplate", base_name.replace(".py", ".c"))
-
-        print "C_file_name", c_file_name
-        self._cache[module_name] = Extension("django_cemplate.{0}".format(base_name[:-3]), sources=[c_file_name], package="django_cemplate")
-        return self._cache[module_name]
-
-    def __getitem__(self, key):
-        module = self.module_list[key]
-        return self._mangle_module(module)
-
-    def __iter__(self):
-        for module in self.module_list:
-            mangled_module = self._mangle_module(module)
-            if mangled_module is not None:
-                yield mangled_module
-
-    def __len__(self):
-        l = len(self.module_list)
-        return l
 
 setup(
     name="django_cemplate",
     author="Alex DAMIAN",
     author_email="ddalex@gmail.com",
-    version="0.1",
+    version="0.98",
     license="GPL",
     url="https://github.com/ddalex/django-cemplate",
     download_url="https://github.com/ddalex/django-cemplate/releases",
@@ -69,5 +24,6 @@ setup(
     packages=["django_cemplate"],
     ext_modules=cythonize("django_cemplate/*.py*"),
     install_requires=['django==1.8.7', 'cython'],
-    scripts=""
+    data_files=[(site_packages_path, ["django_cemplate.pth"])],
+    scripts="",
 )
